8f9d20ab8525232d7351fb2f3ecc36566795bd12
webblaster <yekinnijibola@gmail.com>
2019-05-05 11:31:15 +0100

    fixed functions

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,16 +1,22 @@
 //get dom reference
-var donatebtn = document.querySelector(".donatebtn");
-var volunteerbtn = document.querySelector(".vlrbtn");
+var donatebtn = document.querySelectorAll(".donatebtn");
+var volunteerbtn = document.querySelectorAll(".vlrbtn");
 
 //functions
 var donate = function() {
   window.location.href = "https://paystack.com/pay/tideydonation";
 };
-
 var volunteer = function() {
   window.location.href =
     "https://docs.google.com/forms/d/e/1FAIpQLSfa8B65cx9qOF58PEzww5vFvVvrTvm5d475njVarSfZ62L8VQ/viewform?vc=0&c=0&w=1";
 };
 
-donatebtn.addEventListener("click", donate);
-volunteerbtn.addEventListener("click", volunteer);
+console.log(donatebtn, volunteerbtn);
+//Event listeners
+donatebtn.forEach(function(btn, index) {
+  btn.addEventListener("click", donate);
+});
+
+volunteerbtn.forEach(function(btn, index) {
+  btn.addEventListener("click", volunteer);
+});
